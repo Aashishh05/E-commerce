09b7b53049b29cd21ba533ffea073c1b7a0902ab
Aashishh05 <shresthaaashish246@gmail.com>
2026-06-28 14:49:03 +0545

    added user model for authentication system

---
+++ Backend/models/userModel.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "seller", "buyer"],
+      default: "buyer",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zipCode: String,
+      country: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
