0d4cec2f366093f542c02facd61e31c6dfa58d63
Aashishh05 <shresthaaashish246@gmail.com>
2026-06-28 23:05:38 +0545

    added authentication middleware and request validation system

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  getUser,
+  loginUser,
+  logout,
+  registerUser,
+  updateProfile,
+} from "../controller/authController";
+import { protect } from "../middleware/authMiddleware";
+import { body } from "express-validator";
+import { validate } from "../middleware/validate";
+
+const router = express.Router();
+
+// Validation rules
+const registerValidation = [
+  body("name").trim().notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+  body("role")
+    .isIn(["buyer", "seller"])
+    .withMessage("Role must be buyer or seller"),
+];
+
+const loginValidation = [
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+router.post("/register", registerValidation,validate, registerUser);
+router.post("/login", loginValidation,validate, loginUser);
+router.post("/logout", protect, logout);
+router.get("/me", protect, getUser);
+router.put("/profile", protect, updateProfile);
+
+export default router;
