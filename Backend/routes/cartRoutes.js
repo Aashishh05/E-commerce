d1121c610ba7c0ce108327855b69b4ad4e7011c3
Aashishh05 <shresthaaashish246@gmail.com>
2026-07-02 10:27:32 +0545

    added cart routes with authorization

---
+++ Backend/routes/cartRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  addToCart,
+  clearCart,
+  getCart,
+  removeFromCart,
+  updateCartItem,
+} from "../controller/cartController";
+import { authorize, protect } from "../middleware/authMiddleware";
+
+const router = express.Router();
+
+router.post("/add", protect, authorize("buyer"), addToCart);
+router.get("/get", protect, authorize("buyer"), getCart);
+router.put("/update/:id", protect, authorize("buyer"), updateCartItem);
+router.delete("/remove/:id", protect, authorize("buyer"), removeFromCart);
+router.delete("/clear", protect, authorize("buyer"), clearCart);
+
+export default router;
--- Backend/routes/mainRoutes.js
+++ Backend/routes/mainRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 import authRoutes from "./authRoutes.js";
 import categoryRoutes from "./categoryRoutes.js";
 import productRoutes from "./productRoutes.js";
+import cartRoutes from "./cartRoutes.js"
+
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/category", categoryRoutes);
 router.use("/product", productRoutes);
+router.use("/cart",cartRoutes);
 
 export default router;
