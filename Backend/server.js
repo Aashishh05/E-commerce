dabfa63a5a22958107eb56f43d00e2561da2d69a
Aashishh05 <shresthaaashish246@gmail.com>
2026-07-12 21:20:29 +0545

    setup frontend-backend connection with CORS, Vite proxy, and Axios instance

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,23 @@
-import mainRoutes from "./routes/mainRoutes.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
 import ConnectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import mainRoutes from "./routes/mainRoutes.js";
+import cors from "cors";
 
 ConnectDB();
+
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
