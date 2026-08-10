bc8c55561f3b6bd8203041527864f216756c043d
Aashish Shrestha <shresthaaashish246@gmail.com>
2026-08-10 11:17:00 +0545

    Change API base URL to production environment

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://e-commerce-mhr5.onrender.com",
   withCredentials: true,
 });
 
