89e0e86deb352fa5a8bd05f03e99af98ee7c33f0
Aashish Shrestha <shresthaaashish246@gmail.com>
2026-08-11 22:29:54 +0545

    Update CORS configuration to allow multiple origins

--- Backend/app.js
+++ Backend/app.js
@@ -9,12 +9,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-five-psi-46.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
