0d4b4ac3cfce5fbf1be575d1af08c744125d49d8
Aashishh05 <shresthaaashish246@gmail.com>
2026-08-13 10:42:12 +0545

    fixed

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import toast from "react-hot-toast";
+  console.log("BASE_URL:", import.meta.env.VITE_BASE_URL)
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
